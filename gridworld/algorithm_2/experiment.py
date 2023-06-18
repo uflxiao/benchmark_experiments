d48b3696ea897541692e4e584f89cb61f13d461b
uflxiao <xiaoz@ufl.edu>
2023-06-18 12:13:56 -0400

    REINFORCE algorithm

---
+++ gridworld/algorithm_2/experiment.py
@@ -0,0 +1,13 @@
+from gridworld import GridWorld
+from policy_gradient import PolicyGradient
+from deep_nn_policy import DeepNeuralNetworkPolicy
+
+gridworld = GridWorld(width=3, height=3)
+gridworld.visualise()
+
+policy = DeepNeuralNetworkPolicy(
+    gridworld, state_space=len(gridworld.get_initial_state()), action_space=4
+)
+policy_gradient = PolicyGradient(gridworld, policy, alpha=0.1).execute(episodes=1000)
+gridworld_image = gridworld.visualise_stochastic_policy(policy)
+
